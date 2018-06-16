16707b9e8d1e732f596837d6259de8fc7606b1dc
Florian Frei <flofrei@student.ethz.ch>
2018-06-16 13:02:45 +0200

    reduce max

--- gym_1dmaze/__init__.py
+++ gym_1dmaze/__init__.py
@@ -6,107 +6,107 @@ from gym.envs.registration import register
 register(
     id='1DMaze_1x16_simpleactionset_mode0-v0',
     entry_point='gym_1dmaze.envs:SimpleMaze1x16sasm0',
-    timestep_limit=2000,
+    timestep_limit=100,
 )
 
 register(
     id='1DMaze_1x16_extendedactionset_mode0-v0',
     entry_point='gym_1dmaze.envs:SimpleMaze1x16easm0',
-    timestep_limit=2000,
+    timestep_limit=100,
 )
 
 register(
     id='1DMaze_1x32_simpleactionset_mode0-v0',
     entry_point='gym_1dmaze.envs:SimpleMaze1x32sasm0',
-    timestep_limit=2000,
+    timestep_limit=100,
 )
 
 register(
     id='1DMaze_1x32_extendedactionset_mode0-v0',
     entry_point='gym_1dmaze.envs:SimpleMaze1x32easm0',
-    timestep_limit=2000,
+    timestep_limit=100,
 )
 
 register(
     id='1DMaze_1x16_simpleactionset_mode10-v0',
     entry_point='gym_1dmaze.envs:SimpleMaze1x16sasm10',
-    timestep_limit=2000,
+    timestep_limit=100,
 )
 
 register(
     id='1DMaze_1x16_extendedactionset_mode10-v0',
     entry_point='gym_1dmaze.envs:SimpleMaze1x16easm10',
-    timestep_limit=2000,
+    timestep_limit=100,
 )
 
 register(
     id='1DMaze_1x32_simpleactionset_mode10-v0',
     entry_point='gym_1dmaze.envs:SimpleMaze1x32sasm10',
-    timestep_limit=2000,
+    timestep_limit=100,
 )
 
 register(
     id='1DMaze_1x32_extendedactionset_mode10-v0',
     entry_point='gym_1dmaze.envs:SimpleMaze1x32easm10',
-    timestep_limit=2000,
+    timestep_limit=100,
 )
 
 register(
     id='1DMaze_1x16_simpleactionset_mode42-v0',
     entry_point='gym_1dmaze.envs:SimpleMaze1x16sasm42',
-    timestep_limit=2000,
+    timestep_limit=100,
 )
 
 register(
     id='1DMaze_1x16_extendedactionset_mode42-v0',
     entry_point='gym_1dmaze.envs:SimpleMaze1x16easm42',
-    timestep_limit=2000,
+    timestep_limit=100,
 )
 
 register(
     id='1DMaze_1x32_simpleactionset_mode42-v0',
     entry_point='gym_1dmaze.envs:SimpleMaze1x32sasm42',
-    timestep_limit=2000,
+    timestep_limit=100,
 )
 
 register(
     id='1DMaze_1x32_extendedactionset_mode42-v0',
     entry_point='gym_1dmaze.envs:SimpleMaze1x32easm42',
-    timestep_limit=2000,
+    timestep_limit=100,
 )
 
 register(
     id='1DMaze_1x4_simpleactionset_mode42-v0',
     entry_point='gym_1dmaze.envs:SimpleMaze1x4sasm42',
-    timestep_limit=2000,
+    timestep_limit=100,
 )
 
 register(
     id='1DMaze_1x4_extendedactionset_mode42-v0',
     entry_point='gym_1dmaze.envs:SimpleMaze1x4easm42',
-    timestep_limit=2000,
+    timestep_limit=100,
 )
 
 register(
     id='1DMaze_1x4_simpleactionset_mode0-v0',
     entry_point='gym_1dmaze.envs:SimpleMaze1x4sasm0',
-    timestep_limit=2000,
+    timestep_limit=100,
 )
 
 register(
     id='1DMaze_1x4_extendedactionset_mode0-v0',
     entry_point='gym_1dmaze.envs:SimpleMaze1x4easm0',
-    timestep_limit=2000,
+    timestep_limit=100,
 )
 
 register(
     id='1DMaze_1x10_simpleactionset_mode42-v0',
     entry_point='gym_1dmaze.envs:SimpleMaze1x10sasm42',
-    timestep_limit=2000,
+    timestep_limit=100,
 )
 
 register(
     id='1DMaze_1x10_simpleactionset_mode0-v0',
     entry_point='gym_1dmaze.envs:SimpleMaze1x10sasm0',
-    timestep_limit=2000,
+    timestep_limit=100,
 )
