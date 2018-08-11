86997f98bc4c58ad7c4e38c1be16e769c640d0a1
Florian Frei <flofrei@student.ethz.ch>
2018-08-11 07:51:37 +0200

    remove time pressure

--- gym_1dmaze/envs/advanced_maze_env_key.py
+++ gym_1dmaze/envs/advanced_maze_env_key.py
@@ -367,15 +367,15 @@ class AdvancedMazeKey(gym.Env):
         return np.copy(self.world),r,end_of_eps,lst;
 
     def _automatic_step_returner(self):
-        #r = 0;
-        r = -0.1;
+        r = 0;
+        #r = -0.1;
         end_of_eps = False;
         lst = {-1,self.key_picked_up};
         return np.copy(self.world),r,end_of_eps,lst;
 
     def _automatic_key_returner(self):
-        #r = 0.;
-        r = -0.1;
+        r = 0.;
+        #r = -0.1;
         end_of_eps = False;
         #end_of_eps = True;
         lst = {-1,self.key_picked_up};
