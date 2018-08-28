ca4cf6e650af15ca4de64ac97802a1e0b8efb08d
Florian Frei <flofrei@student.ethz.ch>
2018-08-28 11:12:09 +0200

    more mc experiments

--- gym_1dmaze/envs/advanced_maze_env.py
+++ gym_1dmaze/envs/advanced_maze_env.py
@@ -328,13 +328,13 @@ class AdvancedMaze(gym.Env):
         return np.copy(self.world),r,end_of_eps,lst;
 
     def _automatic_step_returner(self):
-        r = 0.;
+        r = -0.1;
         end_of_eps = False;
         lst = {-1};
         return np.copy(self.world),r,end_of_eps,lst;
 
     def _automatic_wall_returner(self):
-        r = -1.;
+        r = -0.1;
         end_of_eps = True;
         lst = {-1};
         return np.copy(self.world),r,end_of_eps,lst;
