ce94e399d8e0658da3ba6c9c332e98ca777f120d
Florian Frei <flofrei@student.ethz.ch>
2018-09-02 20:53:14 +0200

    reverse time pressure

--- gym_1dmaze/envs/advanced_maze_env_key.py
+++ gym_1dmaze/envs/advanced_maze_env_key.py
@@ -367,8 +367,8 @@ class AdvancedMazeKey(gym.Env):
         return np.copy(self.world),r,end_of_eps,lst;
 
     def _automatic_step_returner(self):
-        #r = 0;
-        r = -0.1;
+        r = 0;
+        #r = -0.1;
         end_of_eps = False;
         lst = {-1,self.key_picked_up};
         return np.copy(self.world),r,end_of_eps,lst;
@@ -383,8 +383,8 @@ class AdvancedMazeKey(gym.Env):
         return np.copy(self.world),r,end_of_eps,lst;
 
     def _automatic_wall_returner(self):
-        #r = 0.;
-        r = -0.1;
+        r = 0.;
+        #r = -0.1;
         end_of_eps = False;
         lst = {-1,self.key_picked_up};
         return np.copy(self.world),r,end_of_eps,lst;
